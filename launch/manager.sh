b5575a005ace60512db6645fa77b8d318bbe168d
Stephanie Martins <s.martins1234@hotmail.com>
2021-05-14 14:32:42 -0600

    add maya stuff

--- launch/manager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $SOURCEDIR/env.sh
-export CURRENT_PROGRAM='manager'
-
-export PYTHONPATH="/opt/hfs17.0.352/python/lib/python3.8/site-packages-ui-forced":${PYTHONPATH}
-
-echo "Starting Manager..."
-python pipe/app/manager/main.py
