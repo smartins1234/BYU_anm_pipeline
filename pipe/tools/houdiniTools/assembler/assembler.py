3efcf255c8ee2eaf6afd03217aadb69066d56142
Jason Williams <jasonwadewilliams1@gmail.com>
2021-06-22 00:46:04 -0600

    assembler progress

--- pipe/tools/houdiniTools/assembler/assembler.py
+++ pipe/tools/houdiniTools/assembler/assembler.py
@@ -2,6 +2,8 @@ import hou, os
 import pipe.pipeHandlers.quick_dialogs as qd
 
 from pipe.pipeHandlers.project import Project
+from pipe.pipeHandlers.body import Body, Asset, Shot, AssetType
+from pipe.pipeHandlers.element import Element
 
 class Assembler:
     def __init__(self):
@@ -10,10 +12,9 @@ class Assembler:
     '''
         Creates new content HDAs
         @param asset_name: name of the asset to assemble/clone
-        @param department_paths: a dictionary of department to existing hda filepaths to clone. None if assembling
         @param already_tabbed_in_node: an hda where the new content hdas should be created. Typically None
     '''
-    def create_hda(self, asset_name, body=None, department_paths=None):
+    def create_hda(self, asset_name, body=None):
         if body is None:
             body = self.body
 
@@ -39,18 +40,46 @@ class Assembler:
 
 
     '''
-        Build the node network for an Asset.
+        Build the node network for an Asset in an HDA subnet.
     '''
     def build_asset(self, body):
         stage = hou.node("/stage")
         temp_node = stage.createNode("subnet")
+        temp_node.deleteItems(temp_node.children())
         asset_name = body.get_name()
 
-        HDA_node = temp_node.createDigitalAsset(
+        HDA = temp_node.createDigitalAsset(
           name = asset_name,
-          hda_file_name = os.path.join(body.get_filepath, "hda", asset_name + ".hdanc"),
-          description = body.get_type,
+          hda_file_name = os.path.join(body.get_filepath(), "hda", asset_name + ".hda"),
+          description = body.get_type(),
           min_num_inputs = 0,
           max_num_inputs = 0,
         )
-        HDA.setName()
+        HDA.setName(asset_name, unique_name=True)
+        HDA.setDisplayFlag(True)
+
+        #clean slate HDA is now created and ready to fill
+        sop_create = HDA.createNode("sopcreate", "import_geo")
+        sop_create.parm("enable_pathattr").set(True)
+        sop_create.parm("enable_kindschema").set(True)
+        sop_create.parm("enable_subsetgroups").set(True)
+        sop_create.parm("kindschema").set("Nested assembly, groups, and components")
+        sop_create.parm("subsetgroups").set("*")
+
+        usd_import = sop_create.children()[0].children()[0].createNode("usdimport")
+        usd_import.parm("input_unpack").set(True)
+        filepath1 = Element(os.path.join(body.get_filepath(), "geo")).get_last_publish()
+        print("\n\nImport filepath:  " + filepath1[3])
+        usd_import.parm("filepath1").set(filepath1[3])
+        wrangle = sop_create.children()[0].children()[0].createNode("attribwrangle")
+        wrangle.setInput(0, usd_import, 0)
+        wrangle.parm("snippet").set("@name = \"/" + asset_name + "\";\n@path = @name;")
+        wrangle.parm("class").set(1)
+
+        output = sop_create.children()[0].children()[0].createNode("output")
+        output.setInput(0, wrangle, 0)
+        output.setDisplayFlag(True)
+        output.setRenderFlag(True)
+
+        childs = sop_create.children()[0].children()[0].children()[0]
+        sop_create.children()[0].children()[0].layoutChildren(childs)
