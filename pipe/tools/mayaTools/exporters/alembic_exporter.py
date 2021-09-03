14d92b6738ba0b0fa6c89fc978703c44fd53bae7
Stephanie Martins <s.martins1234@hotmail.com>
2021-09-02 21:49:47 -0600

    adjusted previs tools to fit department workflow and other bug fixes

--- pipe/tools/mayaTools/exporters/alembic_exporter.py
+++ pipe/tools/mayaTools/exporters/alembic_exporter.py
@@ -176,21 +176,66 @@ class AlembicExporter:
         if self.shot is None:
             return None
 
+        start_frame = 0
+        frame_range = str(self.shot.get_frame_range())
+        #This means there's been no cameras even published for this shot but they're trying to save an animation. shouldn't ever happen
+        if int(frame_range) == 0 and not camera:
+            qd.error("How are you publishing without a camera yet? Get that done, bestie.")
+            return
+        #if this is previs we need to know where to move the keyframes to
         if camera:
+            #ask the artist what frame the shot starts at and make sure that's a valid input
+            start_frame = qd.input("Enter the starting frame for this shot")
+            if start_frame is None or start_frame == u'':
+                qd.error("Invalid frame range. Try publishing again.")
+                return
+            start_frame = str(start_frame)
+            if not start_frame.isdigit():
+                qd.error("Invalid frame range. Try publishing again.")
+                return
+            
+            #ask the artist for the last fram and make sure that's valid input
+            #we'll do this every time in case the length of the shot changes
+            end_frame = qd.input("Enter the last frame for this shot")
+            if end_frame is None or end_frame == u'':
+                qd.error("Invalid frame range. Try publishing again.")
+                return
+            end_frame = str(end_frame)
+            if not end_frame.isdigit():
+                qd.error("Invalid frame range. Try publishing again.")
+                return
+
+            #calculate the frame-range and save it to the shot's .body file
+            frame_range = int(end_frame) - int(start_frame) + 1
+            self.shot.set_frame_range(frame_range)
+
+            #now move the keyframes and build the command
+            timeChange = int(start_frame) - 1
+            anim_curves = mc.ls(type=['animCurveTA', 'animCurveTL', 'animCurveTT', 'animCurveTU'])  
+            for each in anim_curves:  
+                mc.keyframe(each, edit=True, relative=True, timeChange=-timeChange)
+
             path = self.getCameraPath(asset_name)
             command = self.buildAlembicCommand(path, uv=False)
                 
         else:
             path = self.getFilePath(asset_name)
             command = self.buildAlembicCommand(path)
+        
 
-        frame_range = str(self.shot.get_frame_range())
-        cont = qd.yes_or_no("This asset is about to be saved with " + frame_range + " frames. Is that correct?")
+        cont = qd.yes_or_no("This asset is about to be saved with " + str(frame_range) + " frames. Is that correct?")
         if not cont:
             qd.error("Nothing was published.")
             return
 
         pm.Mel.eval(command)
+        
+        #now move the keyframes back for previs
+        if camera:
+            timeChange = int(start_frame) - 1
+            anim_curves = mc.ls(type=['animCurveTA', 'animCurveTL', 'animCurveTT', 'animCurveTU'])  
+            for each in anim_curves:  
+                mc.keyframe(each, edit=True, relative=True, timeChange=timeChange)
 
         publish_info = [self.element, path]
         return publish_info
